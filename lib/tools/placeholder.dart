c74e029448afcabcc8d0488242d15687e2fedfd5
hilltwice <hilltwice@163.com>
2023-11-14 22:35:43 +0800

    root wrapper placeholder

--- lib/tools/placeholder.dart
+++ lib/tools/placeholder.dart
@@ -6,3 +6,21 @@ class CenterText extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Center(child: Text(text));
 }
+
+/// 1. Enable media query properties.
+/// 2. Enable text direction to enable text display.
+class RootWrapper extends StatelessWidget {
+  const RootWrapper({super.key, this.child = const CenterText('app root')});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return MediaQuery(
+      data: const MediaQueryData(),
+      child: Directionality(
+        textDirection: TextDirection.ltr,
+        child: child,
+      ),
+    );
+  }
+}
